235093f760499b58110e04aab494335c81141a9f
Krzysztof Mróz <krzysztofmroz@krzysztofs-mbp.speedport_w_724v_typ_a_05011603_06_003>
2021-05-16 13:58:47 +0200

    windows

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ function StartPage() {
     return (
 <div className='cf'>
     <h1 className='tc'>Polnische Fenster</h1>
-    <div className='fl w-50 w-third-ns w-20-m w-10-l tc pv2 bg-black-20'>1</div>
-    <div className='fl w-50 w-third-ns w-20-m w-10-l tc pv2 bg-black-10'>2</div>
-    <div className='fl w-50 w-third-ns w-20-m w-10-l tc pv2 bg-black-05'>3</div>
-    <div className='fl w-50 w-third-ns w-20-m w-10-l tc pv2 bg-black-10'>4</div>
-    <div className='fl w-50 w-third-ns w-20-m w-10-l tc pv2 bg-black-20'>5</div>
+    <div className='fl w-50   tc pv2 bg-black-20'>1</div>
+    <div className='fl w-50  tc pv2 bg-black-10'>2</div>
+    <div className='fl w-50   tc pv2 bg-black-05'>3</div>
+    <div className='fl w-50  tc pv2 bg-black-10'>4</div>
+    <div className='fl w-50  tc pv2 bg-black-20'>5</div>
 </div>
     );
 
