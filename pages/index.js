730e7072d1f93ced6896cf17f2477421dd477dbd
Krzysztof Mróz <krzysztofmroz@krzysztofs-mbp.speedport_w_724v_typ_a_05011603_06_003>
2021-05-16 13:54:24 +0200

    windows

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import 'tachyons'
 function StartPage() {
     return (
 <div className='cf'>
-    <h1>Polnische Fenster</h1>
-    <div className='fl w-50-ns w-20-m w-10-l tc pv2 bg-black-20'>1</div>
-    <div className='fl w-50-ns w-20-m w-10-l tc pv2 bg-black-10'>2</div>
-    <div className='fl w-50-ns w-20-m w-10-l tc pv2 bg-black-05'>3</div>
-    <div className='fl w-50-ns w-20-m w-10-l tc pv2 bg-black-10'>4</div>
-    <div className='fl w-50-ns w-20-m w-10-l tc pv2 bg-black-20'>5</div>
+    <h1 className='tc'>Polnische Fenster</h1>
+    <div className='fl w-50 w-third-ns w-20-m w-10-l tc pv2 bg-black-20'>1</div>
+    <div className='fl w-50 w-third-ns w-20-m w-10-l tc pv2 bg-black-10'>2</div>
+    <div className='fl w-50 w-third-ns w-20-m w-10-l tc pv2 bg-black-05'>3</div>
+    <div className='fl w-50 w-third-ns w-20-m w-10-l tc pv2 bg-black-10'>4</div>
+    <div className='fl w-50 w-third-ns w-20-m w-10-l tc pv2 bg-black-20'>5</div>
 </div>
     );
 
