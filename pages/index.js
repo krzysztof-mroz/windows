3c2da6ee8774ab53b520a5db83298323eb35454d
Krzysztof Mróz <krzysztofmroz@Krzysztofs-MacBook-Pro.local>
2021-05-16 13:19:11 +0200

    windows

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,14 @@ import 'tachyons'
 
 function StartPage() {
     return (
-<div><h1>Polnische Fenster</h1></div>
+<div className='cf'>
+    <h1>Polnische Fenster</h1>
+    <div className='fl w-50-ns w-20-m w-10-l tc pv2 bg-black-20'>1</div>
+    <div className='fl w-50-ns w-20-m w-10-l tc pv2 bg-black-10'>2</div>
+    <div className='fl w-50-ns w-20-m w-10-l tc pv2 bg-black-05'>3</div>
+    <div className='fl w-50-ns w-20-m w-10-l tc pv2 bg-black-10'>4</div>
+    <div className='fl w-50-ns w-20-m w-10-l tc pv2 bg-black-20'>5</div>
+</div>
     );
 
 };
