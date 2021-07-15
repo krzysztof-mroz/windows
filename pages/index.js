0c1480094c096b8c58bff0c3e7ffeb24cfca7ae0
Krzysztof Mróz <krzysztofmroz@krzysztofs-mbp.speedport_w_724v_typ_a_05011603_06_003>
2021-07-15 13:56:16 +0200

    windows

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,19 @@ function StartPage() {
   return (
     <div>
       <div className="bg-black-50 tc fw9 pb2 pt2">Nav Bar</div>
-      <div className="mh2 mh4-ns mh6-l">
+      <div className="mh2 mh4-ns mh6-l mw9">
         <div className="pa4 tc">Icons, logo, hotline</div>
-        <div className="tc bg-black-20 pa6 br3">IMAGE</div>
+        <div className="tc bg-black-20 pa6 br3 ma2">IMAGE</div>
         <div className="pv3 tl">
-          <h2>Title</h2>
+          <div className="flex flex-wrap justify-around w-100">
+            <h2 className="fl ma2 w-100 w-40-l tl">Title</h2>
+            <p className="fr ma2 w-100 w-40-l tl tr-l">Path</p>
+          </div>
         </div>
 
         <div className="flex flex-wrap justify-center mb5 w-100">
-          <div className="flex  fl w-100 w-third-l">
-            <div className="ba flex justify-center w-100 fl bg-black-10 tc ma2 br3 h5">
+          <div className="flex justify-center  fl w-100 w-third-l">
+            <div className="ba flex justify-center w-100 w-90-l fl bg-black-10 tc ma2 br3 h5">
               card
             </div>
           </div>
@@ -55,10 +58,12 @@ function StartPage() {
             </div>
           </div>
           <div className="flex justify-center fl w-100 w-third-l">
-            <div className="ba w-100 fl bg-black-10 tc ma2 br3 h5">card</div>
+            <div className="ba w-100 w-90-l fl bg-black-10 tc ma2 br3 h5">
+              card
+            </div>
           </div>
         </div>
-        <div className="tc bg-black-10 pa6 br3">FOOTER</div>
+        <div className="tc bg-black-10 pa6 br3 ma2">FOOTER</div>
       </div>
     </div>
   );
