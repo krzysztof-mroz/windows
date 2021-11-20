3fc5590c8b45a19e5bce485a3db9b9c6b5eac1b3
Krzysztof Mróz <krzysztofmroz@Krzysztofs-MacBook-Pro.local>
2021-11-20 17:27:15 +0100

    windows

--- components/ui/profilestrip.js
+++ components/ui/profilestrip.js
@@ -9,7 +9,7 @@ function profileStrip() {
   return (
     <div className="flex flex-wrap justify-center fl w-100 mv2">
 
-      <p className="mr2 mt4"> <b>Bitte Profil wählen:</b></p>
+      <p className="mr2 mt4"> <b>Profil wählen:</b></p>
       {profiles.map((profile) => (
         <div className="tooltip">
           <img
