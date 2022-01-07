1a362559b3bff54a092ecf417ea2dff848f28250
Krzysztof Mróz <krzysztofmroz@Krzysztofs-MacBook-Pro.local>
2022-01-07 20:01:54 +0100

    windows

--- pages/index.js
+++ pages/index.js
@@ -345,6 +345,7 @@ function StartPage() {
                 <ColourStrip
                 onColorChange={changeColor}
                 gasketChange={checkGasket}
+                
               />
               
             </div>
