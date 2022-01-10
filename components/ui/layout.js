38996c8edcb6f29b5dbb4c4be4e648ffff299ba1
Krzysztof Mróz <krzysztofmroz@Krzysztofs-MacBook-Pro.local>
2022-01-10 19:06:46 +0100

    windows

---
+++ components/ui/layout.js
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+
+import NaviBar from "./navibar";
+import FooterDiv from "./footerdiv";
+
+function Layout(props) {
+  return (
+    <Fragment>
+      <div className=" w-100-l">
+        <NaviBar />
+
+        <div
+          className="flex flex-wrap justify-center mh2 mh6-l mw9"
+          style={{ marginTop: "70px" }}
+        >
+          <main>{props.children}</main>
+
+          <FooterDiv />
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,23 @@
+import { Fragment } from "react";
 import "tachyons";
+
 import Description from "../components/ui/description";
-import WarumDiv from "../components/ui/warumdiv";
-import NaviBar from "../components/ui/navibar";
 import HeaderDiv from "../components/ui/headerdiv";
-import FooterDiv from "../components/ui/footerdiv";
+import WarumDiv from "../components/ui/warumdiv";
 import ActionDiv from "../components/ui/actiondiv";
 import Visualisation from "../components/ui/visualisation";
 
 
 function StartPage() {
-  
-   return (
-    <div className=" w-100-l">
-      <NaviBar />
-
-      <div className="flex flex-wrap justify-center mh2 mh6-l mw9" style={{ marginTop: "70px" }} >
-        <HeaderDiv />
-
-        <Visualisation/>
-
-        <Description />
-
-        <WarumDiv />
-
-        <ActionDiv />
-
-        <FooterDiv />
-
-        
-      </div>
-    </div>
+  return (
+    <Fragment>
+      <HeaderDiv />
+      <Visualisation />
+      <Description />
+      <WarumDiv />
+      <ActionDiv />
+    </Fragment>
   );
-
-  
 }
 
 export default StartPage;
