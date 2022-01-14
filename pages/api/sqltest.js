66a77a69d6c384892403ca7dc117b3fb7aad5085
Krzysztof Mróz <krzysztofmroz@Krzysztofs-MacBook-Pro.local>
2022-01-14 17:23:18 +0100

    windows

--- pages/api/sqltest.js
+++ pages/api/sqltest.js
@@ -2,10 +2,8 @@ import {sql_query} from "../lib/db"
 
 const handler = async (_, res) => {
     try {
-        const result = await sql_query(
-            'SELECT * FROM test'
-          );
-        return res.json(results);
+        const result = await sql_query('SELECT * FROM test');
+        return res.json(result);
         
     } catch(e) {
         res.status(500).json({message: e.message});
--- pages/lib/db.js
+++ pages/lib/db.js
@@ -4,7 +4,7 @@ import mysql from 'serverless-mysql'
 export const db = mysql({
     config: {
       host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
+      ort: process.env.MYSQL_PORT,
       database: process.env.MYSQL_DATABASE,
       user: process.env.MYSQL_USERNAME, 
       password: process.env.MYSQL_PASSWORD,
