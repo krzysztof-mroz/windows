3c67b251336f6e9002ecc061a553be44371b5d6f
Krzysztof Mróz <krzysztofmroz@Krzysztofs-MacBook-Pro.local>
2022-01-16 18:45:47 +0100

    windows

--- pages/lib/db.js
+++ pages/lib/db.js
@@ -1,4 +1,21 @@
-import mysql from 'serverless-mysql';
+function db () {
+
+const mysql = require('serverless-mysql')({
+  config: {
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    port: parseInt(process.env.MYSQL_PORT),
+  }
+})
+
+ return (
+   <div> test </div>
+ )
+}
+
+/*import fs from 'fs';
 
 export const db = mysql({
   config: {
@@ -21,4 +38,5 @@ export async function query(
   } catch (e) {
     throw Error(e.message)
   }
-}
+}*/
+export default db;
