3d855a2c17768268ce9fe42e96c46d56aaf66925
Krzysztof Mróz <krzysztofmroz@krzysztofs-mbp.speedport_w_724v_typ_a_05011603_06_003>
2022-01-21 19:57:45 +0100

    windows

--- components/ui/fensterdiv.js
+++ components/ui/fensterdiv.js
@@ -1,11 +1,13 @@
 function fensterdiv() {
+
+  function clickHandler(e) {
+    console.log(e.target.title)
+  }
+
   return (
     <div className="flex flex-wrap justify-around  w-100  tc mb1 mt3 ">
-     {/* <div className="w-100 ba b--moon-gray mb4">
-        <h4>Warum Polnische-Fenster.com</h4>{" "}
-  </div>*/}
 
-      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3">
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3 dim" title="schuecopvc" onMouseDown={clickHandler}>
         <img src="./pics/icons/livingfeather.png"></img>
         <h5>Schüco Kunststoff Fenster</h5>
         <p className="gray f6">
@@ -13,7 +15,7 @@ function fensterdiv() {
         </p>
       </div>
 
-      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3">
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3 dim">
         <img src="./pics/icons/k88feather.png"></img>
         <h5>Kömmerling Kunststoff Fenster</h5>
         <p className="gray f6">
@@ -24,22 +26,22 @@ function fensterdiv() {
       <div className="dn db-l w-100 w-30-l  b--moon-gray ma2 pa3"> 
       </div>
       
-      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3">
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3 dim">
         <img src="./pics/icons/pe78feather.png"></img>
         <h5>Ponzio Aluminium Fenster</h5>
         <p className="gray f6">
           Aluminium Fenster zum vernünftigen Preis mit 3 Dichtungen und mit thermischer Trennung.
         </p>
       </div>
-      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3">
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3 dim">
         <img src="./pics/icons/mb86feather.png"></img>
         <h5>Aluprof Aluminium Fenster</h5>
         <p className="gray f6">
           Aluminium Fenster mit geringer Profilhöhe und hoher Gestaltungsfreiheit. 
         </p>
       </div>
-      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3">
-        <img src="./pics/icons/aws75feather.png"></img>
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3 dim">
+        <img src="./pics/icons/aws90feather.png"></img>
         <h5>Schüco Aluminium Fenster</h5>
         <p className="gray f6">
           Aluminium Fenster und Türen für die höchsten Ansprüche, ohne Kompromisse.
