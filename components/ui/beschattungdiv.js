c30a21a53d54dc3e0c93ef6306631b48f76cdc4d
Krzysztof Mróz <krzysztofmroz@krzysztofs-mbp.speedport_w_724v_typ_a_05011603_06_003>
2022-01-23 20:46:58 +0100

    windows

--- components/ui/beschattungdiv.js
+++ components/ui/beschattungdiv.js
@@ -6,9 +6,9 @@ function beschattungdiv() {
   function gotoVeyna(e) {
     // router.push("./schuecopvc")
   }
-  function gotoPerito(e) {}
-  function gotoVeynaAlu(e) {}
-  function gotoAluprof(e) {}
+  function gotoRollos(e) {}
+  function gotoRaf(e) {}
+  function gotoScreen(e) {}
 
   return (
     <div className="flex flex-wrap justify-around  w-100  tc mb1 mt3 ">
@@ -16,12 +16,12 @@ function beschattungdiv() {
 
       <div
         className="w-100 w-30-l ba b--moon-gray ma2 pa3 dim"
-        onMouseDown={gotoVeyna}
+        onMouseDown={gotoRollos}
       >
         <img src="./pics/icons/rollo.png"></img>
         <h5>Rollläden</h5>
         <p className="gray f6">
-          
+          Standard Beschattung mit Aluminium Lamellen. Mit Gurt oder Motor. Als Aufsatz- oder Vorsatzrollläden.
         </p>
       </div>
 
@@ -29,23 +29,23 @@ function beschattungdiv() {
 
       <div
         className="w-100 w-30-l ba b--moon-gray ma2 pa3 dim"
-        onMouseDown={gotoPerito}
+        onMouseDown={gotoRaf}
       >
-        <img src="./pics/icons/per.jpg"></img>
-        <h5>Raffstoren</h5>
+        <img src="./pics/icons/rafstore.png"></img>
+        <h5>Außen Raffstoren</h5>
         <p className="gray f6">
-         
+        Raffstoren mit C-80 oder Z-90 Lamellen und Motorsteuerung. Für stufenlose Regulierung des Tageslichtes im Raum.
         </p>
       </div>
 
       <div
         className="w-100 w-30-l ba b--moon-gray ma2 pa3 dim"
-        onMouseDown={gotoVeynaAlu}
+        onMouseDown={gotoScreen}
       >
-        <img src="./pics/icons/veyna.png"></img>
-        <h5>Screens</h5>
+        <img src="./pics/icons/screenroll.png"></img>
+        <h5>"Screen Roll" Rollläden</h5>
         <p className="gray f6">
-          
+        Moderne Lösung, jetzt auch für Außen. Kleine Kästen und effektive Beschattung.
         </p>
       </div>
       
