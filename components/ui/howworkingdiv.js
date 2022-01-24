68e8fc2659505a2335158f3de1798534465f6b0a
Krzysztof Mróz <krzysztofmroz@krzysztofs-mbp.speedport_w_724v_typ_a_05011603_06_003>
2022-01-24 16:03:04 +0100

    windows

---
+++ components/ui/howworkingdiv.js
@@ -0,0 +1,125 @@
+
+function howworkingdiv() {
+  
+
+  
+
+  return (
+    <div className="flex flex-wrap justify-around  w-100  tc mb1 mt3 ">
+      
+
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 1)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/computer.svg"></img>
+        <h5>Fenster finden</h5>
+        <p className="gray f6">
+          Nutzen Sie unsere 3D Visualisierung und Informationen um entsprechende Fenster zu finden. Egal ob Kunststoff oder Aluminium, weiß oder farbig. Mit Rollladen, oder ohne.
+        </p>
+      </div>
+
+      
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 2)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/chat.svg"></img>
+        <h5>Fragen stellen</h5>
+        <p className="gray f6">
+          Stellen Sie Ihre Fragen. Wie es Ihnen einfacher ist. Per WhatsApp, Email oder telefonisch. Wir beraten Sie gerne. Kontaktdaten finden Sie oben. 
+        </p>
+      </div>
+
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 3)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/angebot.svg"></img>
+        <h5>Angebot anfordern</h5>
+        <p className="gray f6">
+          Sie wissen schon, was Sie wollen. Schicken Sie uns Ihre Fensterliste, nutzen Sie alternativ unseres Anfrageformular, oder Konfigurator. Dann ist der Ball bei uns.
+        </p>
+      </div>
+
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 4)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/hersteller.svg"></img>
+        <h5>Hersteller finden</h5>
+        <p className="gray f6">
+          Wir schicken Ihnen ein Angebot. Betreiben Sie eine Firma mit USt. Id Nr? Dann, wenn gewünscht, werden wir Sie gerne direkt mit dem Hersteller kontaktieren.
+        </p>
+      </div>
+
+      
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 5)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/vertrag.svg"></img>
+        <h5>Vertrag machen</h5>
+        <p className="gray f6">
+          Sagt Ihnen das Angebot zu? Wir (bzw. der Hersteller) machen mit Ihnen den Vertrag fest. 
+        </p>
+      </div>
+
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 6)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/zahlung.svg"></img>
+        <h5>Anzahlung leisten</h5>
+        <p className="gray f6">
+          Vertrag abgeschlossen? Sie leisten dann die Anzahlung nach Vereinbarung. Kontaktieren Sie uns bitte zum Thema Absicherung Ihrer Anzahlung bei höheren Beträgen.
+        </p>
+      </div>
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 7)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/aufmass.svg"></img>
+        <h5>Aufmaß machen</h5>
+        <p className="gray f6">
+          Wenn Sie wegen den Maßen immer noch nicht sicher sind, kommen wir gerne bei Ihnen vorbei und messen alles von A bis Z. 
+        </p>
+      </div>
+
+      
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 8)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/auftrag.svg"></img>
+        <h5>Auftrag bestätigen</h5>
+        <p className="gray f6">
+          Jetzt haben wir alle Bestellmasse. Falls nötig, werden wir den Auftrag anpassen und Ihnen zur Bestätigung schicken.
+        </p>
+      </div>
+
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 9)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/kalender.svg"></img>
+        <h5>Lieferzeit abwarten</h5>
+        <p className="gray f6">
+          Na ja, jetzt muss man geduldig abwarten. Beachten Sie unsere Lieferzeiten bei dem bestellten Profil. 
+        </p>
+      </div>
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 10)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/herstellung.svg"></img>
+        <h5>Herstellung der Fenster</h5>
+        <p className="gray f6">
+          Ihre Fenster, Türen und Rollläden werden produziert. Wir informieren Sie laufend wegen dem Produktionsstatus.
+        </p>
+      </div>
+
+      
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 11)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/transport.svg"></img>
+        <h5>Transport zu Ihrer Tür</h5>
+        <p className="gray f6">
+          Ca. 2 Wochen vor dem Transport werden Sie darüber informiert. Sind Restbeträge noch zu leisten? Machen Sie es bitte jetzt.
+        </p>
+      </div>
+
+      <div className="w-100 w-30-l ba b--moon-gray ma2 pa3" >
+        <h5>Schritt 12)</h5>
+        <img style={{ position: "relative", width: 100, height: 100 }} src="./pics/svg/montage.svg"></img>
+        <h5>Fenster Montage</h5>
+        <p className="gray f6">
+          Falls Sie Montage / Demontage bei uns bestellt haben, werden Sie wegen dem Termin früher informiert und somit haben Sie die ganze Leistung "unter einem Dach".
+        </p>
+      </div>
+      
+    </div>
+  );
+}
+
+export default howworkingdiv;
--- pages/howworking.js
+++ pages/howworking.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import HeaderDiv from "../components/ui/headerdiv";
+import HowWorkingDiv from "../components/ui/howworkingdiv";
 import Head from 'next/head';
 
 function HowWorking() {
@@ -16,13 +17,15 @@ function HowWorking() {
             content="fenster, haustür, rollladen, raffstore, polnische fenster"
           />
         </Head>
-        <HeaderDiv title="Fenster aus Polen"/>
+        <HeaderDiv title="12 Schritte noch zu Ihren Fenstern"/>
         <div className="flex flex-wrap justify-around  w-100  tc mb1 mt3 ">
-            <p>Wir helfen Ihnen gerne bei der Fensterauswahl. Wir bearten Sie, egal ob Sie Kuststoff- oder Aluminium Fenster brauchen. Starten Sie zuerst Ihr Abenteuer mit unserer 3D Visualisierung. Jederzeit können Sie uns dann kontaktieren, falls Sie Hilfe brauchen. Egal ob telefonisch, per Email oder WhatsApp. </p>
+           
+            <HowWorkingDiv />
             <div className="w-100 ba b--moon-gray ma2 pa3 ">
             <img src="./pics/jugl_lieferung.png"></img>
             </div>
         </div>
+       
       </Fragment>
     )
         
