86db43de74ad2ac11cd43483a246eae4e3355720
Krzysztof Mróz <krzysztof.mroz@me.com>
2022-03-25 13:38:14 +0100

    loader2

--- components/ui/buildingvisualisation.js
+++ components/ui/buildingvisualisation.js
@@ -81,7 +81,7 @@ function buildingvisualisation({...props}) {
                     <Background />
                   </Suspense>
 
-                  <Suspense fallback={null}>
+                  <Suspense fallback={<Loader />}>
 
                  
 
