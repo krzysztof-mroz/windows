7caba1a7f927a19663175679368cbd4e17ae512c
Krzysztof Mróz <krzysztof.mroz@me.com>
2022-05-17 20:10:47 +0200

    schueco pvc carousel

--- components/ui/schuecopvcdiv.js
+++ components/ui/schuecopvcdiv.js
@@ -1,4 +1,65 @@
+import {useState, useEffect } from "react";
+import Carousel from "./blocks/carousel";
+import ProductLink from "./blocks/productlink";
+
 function schuecopvcdiv() {
+
+  const size = useWindowSize();
+
+  function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+    useEffect(() => {
+      // only execute all the code below in client side
+      if (typeof window !== "undefined") {
+        // Handler to call on window resize
+        function handleResize() {
+          // Set window width/height to state
+          setWindowSize({
+            width: 1536 > window.innerWidth ? window.innerWidth : 1536,
+            height: window.innerHeight,
+          });
+        }
+
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+      }
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
+  }
+
+  const familyCT70Classic = [
+  <ProductLink linktext="Schüco CT 70 Classic" href="/products/ct70" opis="Kunststoff 5 Kammer, 70 mm Einbautiefe" pic="/pics/producticons/ct70_classic.png"/>,
+  <ProductLink linktext="Schüco CT 70 Nebeneingangstür" href="/products/ct70nt" opis="Kunststoff Tür, 70 mm Einbautiefe" pic="/pics/producticons/ct70_nt.png"/>,
+  <ProductLink linktext="Schüco CT 70 Haustür" href="/products/ct70ht" opis="Kunststoff Haustür, 70 mm Einbautiefe" pic="/pics/producticons/ct70_ht.png"/>,
+  <ProductLink linktext="Schüco LivingSlide" href="/hs" opis="Hebe Schiebe, flache Schwelle" pic="/pics/producticons/hs_ls_weiss.png"/>,
+  ];
+
+  const familyCT70Rondo = [
+    <ProductLink linktext="Schüco CT 70 Rondo" href="/products/ct70rondo" opis="Kunststoff 5 Kammer, 70 mm Einbautiefe" pic="/pics/producticons/ct70_rondo.png"/>,
+    <ProductLink linktext="Schüco CT 70 Nebeneingangstür" href="/products/ct70nt" opis="Kunststoff Tür, 70 mm Einbautiefe" pic="/pics/producticons/ct70_nt.png"/>,
+    <ProductLink linktext="Schüco CT 70 Haustür" href="/products/ct70ht" opis="Kunststoff Haustür, 70 mm Einbautiefe" pic="/pics/producticons/ct70_ht.png"/>,
+    <ProductLink linktext="Schüco LivingSlide" href="/hs" opis="Hebe Schiebe, flache Schwelle" pic="/pics/producticons/hs_ls_weiss.png"/>,
+  ];
+
+  const familyLiving = [
+    <ProductLink linktext="Schüco Living MD" href="/products/living" opis="Kunststoff 7 Kammer, 82 mm Einbautiefe" pic="/pics/producticons/living.png"/>,
+    <ProductLink linktext="Schüco Living Nebeneingangstür" href="/products/livingnt" opis="Kunststoff Tür, 82 mm Einbautiefe" pic="/pics/producticons/living_nt.png"/>,
+    <ProductLink linktext="Schüco Living Haustür" href="/products/livinght" opis="Kunststoff Haustür, 82 mm Einbautiefe" pic="/pics/producticons/living_ht.png"/>,
+    <ProductLink linktext="Schüco LivingSlide" href="/hs" opis="Hebe Schiebe, flache Schwelle" pic="/pics/producticons/hs_ls_weiss.png"/>,
+  ];
+
+  
+
+
   return (
     <div className="flex flex-wrap justify-around  w-100  tc mb1 mt3 ">
       <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
@@ -44,6 +105,12 @@ function schuecopvcdiv() {
             src="./pics/sections/ct70classic.png"
           ></img>
         </div>
+        <Carousel
+        show={Math.floor((size.width - 50) / 186)}
+        title="Produktfamilie:"
+      >
+        {familyCT70Classic}
+      </Carousel>
       </div>
 
       <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
@@ -92,6 +159,12 @@ function schuecopvcdiv() {
             src="./pics/sections/ct70rondo.png"
           ></img>
         </div>
+        <Carousel
+        show={Math.floor((size.width - 50) / 186)}
+        title="Produktfamilie:"
+      >
+        {familyCT70Rondo}
+      </Carousel>
       </div>
 
       <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
@@ -137,6 +210,12 @@ function schuecopvcdiv() {
             src="./pics/sections/living.png"
           ></img>
         </div>
+        <Carousel
+        show={Math.floor((size.width - 50) / 186)}
+        title="Produktfamilie:"
+      >
+        {familyLiving}
+      </Carousel>
       </div>
     </div>
   );
