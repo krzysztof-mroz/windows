fb00ed8a995f5b8f9fc9bce56a314689762d08bb
Krzysztof Mróz <krzysztof.mroz@me.com>
2022-05-18 14:25:53 +0200

    kontakt

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -1,11 +1,22 @@
 import { Fragment } from "react";
 import HeaderDiv from "../components/ui/headerdiv";
 import Schnellkontakt from "../components/ui/schnellkontakt";
+import Head from "next/head";
 
 function Kontakt() {
   return (
     <Fragment>
-      <HeaderDiv />
+      <Head>
+        <title>Polnische Fenster Kunststoff und Aluminium</title>
+        <meta name='description'
+              content='Schüco, Kömmerling, Ponzio und Aluprof Fenster aus Polen mit Montage. 3d Visualisierung'
+        />
+         <meta name='keywords'
+              content='fenster, kunststofffenster, fenster aus polen, schüco, kömmerling, ponzio, aluprof, fenster konfigurator'
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <HeaderDiv title="Kontakt"/>
       <div className="flex flex-wrap justify-around mv1 tc fl w-100 ba b--moon-gray pa4">
         <Schnellkontakt />
       </div>
