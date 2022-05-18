951d6f02d3ce3c21d497e00383d4548770db22df
Krzysztof Mróz <krzysztof.mroz@me.com>
2022-05-18 13:12:15 +0200

    koemmerling carousel

--- components/ui/koemmerlingdiv.js
+++ components/ui/koemmerlingdiv.js
@@ -1,4 +1,69 @@
+import {useState, useEffect } from "react";
+import Carousel from "./blocks/carousel";
+import ProductLink from "./blocks/productlink";
+
 function koemmerlingdiv() {
+
+  const size = useWindowSize();
+
+  function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+    useEffect(() => {
+      // only execute all the code below in client side
+      if (typeof window !== "undefined") {
+        // Handler to call on window resize
+        function handleResize() {
+          // Set window width/height to state
+          setWindowSize({
+            width: 1536 > window.innerWidth ? window.innerWidth : 1536,
+            height: window.innerHeight,
+          });
+        }
+
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+      }
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
+  }
+
+  const familyK70 = [
+    <ProductLink linktext="Kömmerling 70 AD" href="/products/k70" opis="Kunststoff 5 Kammer, 70 mm Einbautiefe" pic="/pics/producticons/k70.png"/>,
+    <ProductLink linktext="Kömmerling 70 Nebeneingangstür" href="/products/k70nt" opis="Kunststoff Tür, 70 mm Einbautiefe" pic="/pics/producticons/k70_nt.png"/>,
+    <ProductLink linktext="Kömmerling Premidoor 88" href="/hspd" opis="Hebe Schiebe, flache Schwelle" pic="/pics/producticons/hs_pd.png"/>,
+  ];
+
+  const familyK76AD = [
+    <ProductLink linktext="Kömmerling 76 AD" href="/products/k76ad" opis="Kunststoff 5 Kammer, 76 mm Einbautiefe" pic="/pics/producticons/k76_ad.png"/>,
+    <ProductLink linktext="Kömmerling 76 Nebeneingangstür" href="/products/k76nt" opis="Kunststoff Tür, 76 mm Einbautiefe" pic="/pics/producticons/k76_nt.png"/>,
+    <ProductLink linktext="Kömmerling 76 Haustür" href="/products/k76ht" opis="Kunststoff Haustür, 76 mm Einbautiefe" pic="/pics/producticons/k76_ht.png"/>,
+    <ProductLink linktext="Kömmerling Premidoor 88" href="/hspd" opis="Hebe Schiebe, flache Schwelle" pic="/pics/producticons/hs_pd.png"/>,
+  ];
+
+  const familyK76MD = [
+    <ProductLink linktext="Kömmerling 76 MD" href="/products/k76md" opis="Kunststoff 6 Kammer, 76 mm Einbautiefe" pic="/pics/producticons/k76_md.png"/>,
+    <ProductLink linktext="Kömmerling 76 Nebeneingangstür" href="/products/k76nt" opis="Kunststoff Tür, 76 mm Einbautiefe" pic="/pics/producticons/k76_nt.png"/>,
+    <ProductLink linktext="Kömmerling 76 Haustür" href="/products/k76ht" opis="Kunststoff Haustür, 76 mm Einbautiefe" pic="/pics/producticons/k76_ht.png"/>,
+    <ProductLink linktext="Kömmerling Premidoor 88" href="/hspd" opis="Hebe Schiebe, flache Schwelle" pic="/pics/producticons/hs_pd.png"/>,
+  ];
+
+  const familyK88 = [
+    <ProductLink linktext="Kömmerling 88 MD" href="/products/k88" opis="Kunststoff 7 Kammer, 88 mm Einbautiefe" pic="/pics/producticons/k88.png"/>,
+    <ProductLink linktext="Kömmerling 88 Nebeneingangstür" href="/products/k88nt" opis="Kunststoff Tür, 88 mm Einbautiefe" pic="/pics/producticons/k88_nt.png"/>,
+    <ProductLink linktext="Kömmerling 88 Haustür" href="/products/k88ht" opis="Kunststoff Haustür, 88 mm Einbautiefe" pic="/pics/producticons/k88_ht.png"/>,
+    <ProductLink linktext="Kömmerling Premidoor 88" href="/hspd" opis="Hebe Schiebe, flache Schwelle" pic="/pics/producticons/hs_pd.png"/>,
+  ];
+
+
   return (
     <div className="flex flex-wrap justify-around  w-100  tc mb1 mt3 ">
       <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
@@ -30,6 +95,7 @@ function koemmerlingdiv() {
               <li>Mehrfachspaltlüftung und Flügelheber im Standard,</li>
             </ul>
           </div>
+         
         </div>
 
         <div className="w-100 w-50-l ma1 pa2">
@@ -44,6 +110,12 @@ function koemmerlingdiv() {
             src="./pics/sections/k70.png"
           ></img>
         </div>
+        <Carousel
+        show={Math.floor((size.width - 50) / 186)}
+        title="Produktfamilie:"
+      >
+        {familyK70}
+      </Carousel>
       </div>
 
       <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
@@ -87,6 +159,12 @@ function koemmerlingdiv() {
             src="./pics/sections/k76ad.png"
           ></img>
         </div>
+        <Carousel
+        show={Math.floor((size.width - 50) / 186)}
+        title="Produktfamilie:"
+      >
+        {familyK76AD}
+      </Carousel>
       </div>
 
       <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
@@ -134,6 +212,12 @@ function koemmerlingdiv() {
             src="./pics/sections/k76md.png"
           ></img>
         </div>
+        <Carousel
+        show={Math.floor((size.width - 50) / 186)}
+        title="Produktfamilie:"
+      >
+        {familyK76MD}
+      </Carousel>
       </div>
 
       <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
@@ -181,6 +265,12 @@ function koemmerlingdiv() {
             src="./pics/sections/k88.png"
           ></img>
         </div>
+        <Carousel
+        show={Math.floor((size.width - 50) / 186)}
+        title="Produktfamilie:"
+      >
+        {familyK88}
+      </Carousel>
       </div>
     </div>
   );
