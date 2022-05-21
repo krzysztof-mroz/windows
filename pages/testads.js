21da0e6651430a690e349aa6c92e1ad8a12d60f1
Krzysztof Mróz <krzysztof.mroz@me.com>
2022-05-21 13:08:20 +0200

    index check 20

--- pages/testads.js
+++ pages/testads.js
@@ -133,68 +133,8 @@ const hebeschiebe = [
 
      
      
-      <div className="flex flex-wrap justify-around  w-100  tc mb1 mt3">
-     {/* <div className="w-100 ba b--moon-gray mb4">
-        <h4>Warum Polnische-Fenster.com</h4>{" "}
-  </div>*/}
-
-      <div className="w-100 w-30-l  b--moon-gray ma2 pa3 ba">
-        <img src="/pics/ikona_markenware.png"></img>
-        <h5>Nur Markenware</h5>
-        <p className="gray f6">
-          Profile von Schüco, Profine und Ponzio, Beschläge von Winkhaus mit
-          Pilzkopf Verriegelung, Griffe von Hoppe. Alles vom feinsten mit bester
-          Verarbeitungsqualität.
-        </p>
-      </div>
-
-      <div className="w-100 w-30-l  b--moon-gray ma2 pa3 ba">
-        <img src="/pics/ikona_idea.png"></img>
-        <h5>Kompetente Beratung</h5>
-        <p className="gray f6">
-          Wir beraten Sie gerne im Bereich der neuen Fenster, sowie Beschattung.
-          Auch bei Ihnen vor Ort. Erfahrenes Personal sorgt für Erfüllung der
-          Kundenwünsche.
-        </p>
-      </div>
-      <div className="w-100 w-30-l  b--moon-gray ma2 pa3 ba">
-        <img src="/pics/ikona_aufmass.png"></img>
-        <h5>Aufmaß vor Ort</h5>
-        <p className="gray f6">
-          Kein Fensterprofi? Was für ein Zufall. Wir übernehmen Aufmaß vor Ort
-          für Sie, sowie klären die benötigten Details mit Ihrem Architekten /
-          Bauleiter.
-        </p>
-      </div>
-      <div className="w-100 w-30-l  b--moon-gray ma2 pa3 ba">
-        <img src="/pics/ikona_transport.png"></img>
-        <h5>Schnelle Lieferung</h5>
-        <p className="gray f6">
-          Kurze Lieferzeiten. Anlieferung zu Ihrer Tür oder auf die Baustelle,
-          auf Wunsch Autos mit Gabelstapler. Lieferung auf Stahl- oder
-          Holzgestellen nach Absprache.
-        </p>
-      </div>
-      <div className="w-100 w-30-l  b--moon-gray ma2 pa3 ba">
-        <img src="/pics/ikona_montage.png"></img>
-        <h5>Montage nach RAL</h5>
-        <p className="gray f6">
-          Für unsere Kunden übernehmen wir den Einbau nach RAL im Altbau (mit
-          Demontage), sowie im Neubau. Alles unter einem Dach. Bitte die Montage
-          anfragen.
-        </p>
-      </div>
-      <div className="w-100 w-30-l  b--moon-gray ma2 pa3 ba">
-        <img src="/pics/ikona_garantie.png"></img>
-        <h5>5 Jahre Garantie</h5>
-        <p className="gray f6">
-          Sie bekommen Garantie für unsere Fenster für 5 Jahre. Es gilt auch für
-          die Montage, solange wir Ihre Fenster montiert haben. Was brauchen Sie
-          mehr?
-        </p>
-      </div>
-    </div>
-    
+      <WarumDiv />
+      <ActionDiv /> 
      
     </Fragment>
   );
