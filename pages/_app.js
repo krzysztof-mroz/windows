9fe293ebf22fd456fd94fbad87653881d1231614
Krzysztof Mróz <krzysztof.mroz@me.com>
2022-06-08 11:44:10 +0200

    googla an 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,13 @@
 import Layout from '../components/ui/layout'
 import '../styles/globals.css'
-import Script from "next/script";
+
 
 
 function MyApp({ Component, pageProps }) {
   return (
     
   <Layout>
-    <Script
-    strategy="lazyOnload"
-    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-  />
-
-  <Script id="google-analytics" strategy="lazyOnload">
-    {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-                `}
-  </Script>
+   
     <Component {...pageProps} />
   </Layout>
        
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,25 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="de">
-    <Head />
+    <Head>
+    
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-EPGNMCECXW"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EPGNMCECXW', { page_path: window.location.pathname });
+            `,
+          }}
+        />
+      
+    </Head>
       <body>
         <Main />
         <NextScript />
