6b8c89eb25982ce8b6661bca2989be0cd220fb1b
Krzysztof Mróz <krzysztof.mroz@me.com>
2022-06-08 12:10:52 +0200

    google tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+
 
 export default function Document() {
   return (
     <Html lang="de">
     <Head>
     
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-EPGNMCECXW"
-        />
+    <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-52B2G4Q');`}}></Script>
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-EPGNMCECXW', { page_path: window.location.pathname });
-            `,
-          }}
-        />
+
       
     </Head>
       <body>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52B2G4Q"
+height="0" width="0" style={{display:'none', visibility:'hidden'}}></iframe></noscript>
         <Main />
         <NextScript />
       </body>
