be659a32819a80b5282bf0d1fb50ccf5fab4f6b9
Krzysztof Mróz <krzysztof.mroz@me.com>
2024-01-15 20:23:00 +0100

    Kopplung Verbreiterung

--- pages/layout.js
+++ pages/layout.js
@@ -3,40 +3,104 @@ import React, { Fragment, useState, useEffect, useRef } from "react";
 import Head from "next/head";
 import HeaderDiv from "../components/ui/headerdiv";
 
+function drawRectangle(ctx, posX, posY, width, height, scaleFactor, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(posX, posY, width * scaleFactor, height * scaleFactor);
+  ctx.strokeRect(posX, posY, width * scaleFactor, height * scaleFactor);
+}
+
 // define the Einheit (Fenster, Rahmen) class
 class Einheit {
-  constructor(width, height) {
+  constructor(width, height, up = [], down = [], left = [], right = []) {
     this.width = width;
     this.height = height;
+    this.up = Array.isArray(up) ? up : [];
+    this.down = Array.isArray(down) ? down : [];
+    this.left = Array.isArray(left) ? left : [];
+    this.right = Array.isArray(right) ? right : [];
   }
 
+  get netWidth() {
+    const totalWidthReduction = this.left.concat(this.right).reduce((sum, { width = 0 }) => sum + width, 0);
+    return this.width - totalWidthReduction;
+  }
+
+  get netHeight() {
+    const totalHeightReduction = this.up.concat(this.down).reduce((sum, { height = 0 }) => sum + height, 0);
+    return this.height - totalHeightReduction;
+  }
+
+  
+
   drawEinheit(posX, posY, canvas, scaleFactor) {
+
+    console.log("width" + this.netWidth)
+    console.log("height" + this.netHeight)
+
     const ctx = canvas.getContext("2d");
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let actualX = posX;
+    let actualY = posY;
+  
+   
+      // Calculate scaling factor
+      let realWidth = this.width * scaleFactor;
+      let realHeight = this.height * scaleFactor;
+      let realNetWidth = this.netWidth * scaleFactor;
+      let realNetHeight = this.netHeight * scaleFactor;
+      
+      // upper objects draw
+      this.up.forEach(obj => {
+        drawRectangle(ctx, actualX, actualY, this.width, obj.height, scaleFactor, "white");
+        actualY += obj.height * scaleFactor
+      });
 
-    // Calculate scaling factor
-    let realWidth = this.width * scaleFactor;
-    let realHeight = this.height * scaleFactor;
+      // left objects draw: 
+      this.left.forEach(obj => {
+        drawRectangle(ctx, actualX, actualY, obj.width, this.netHeight, scaleFactor, "white");
+        actualX += obj.width * scaleFactor
+      });  
 
-    // Draw the first (white) rectangle
-    ctx.fillStyle = "white";
-    ctx.fillRect(posX, posY, realWidth, realHeight);
-    ctx.strokeRect(posX, posY, realWidth, realHeight);
 
-    // Calculate dimensions for the second (blue) rectangle
-    let innerWidth = Math.max(0, realWidth - 140 * scaleFactor);
-    let innerHeight = Math.max(0, realHeight - 140 * scaleFactor);
-    let innerPosX = posX + 70 * scaleFactor;
-    let innerPosY = posY + 70 * scaleFactor;
+      // Draw window
+      ctx.fillStyle = "white";
+      ctx.fillRect(actualX, actualY, realNetWidth, realNetHeight);
+      ctx.strokeRect(actualX, actualY, realNetWidth, realNetHeight);
 
-    // Draw the second (blue) rectangle
-    ctx.fillStyle = "blue";
-    ctx.fillRect(innerPosX, innerPosY, innerWidth, innerHeight);
-    ctx.strokeRect(innerPosX, innerPosY, innerWidth, innerHeight);
+      // Calculate dimensions for the second (blue) rectangle
+      let innerWidth = Math.max(0, realNetWidth - 140 * scaleFactor);
+      let innerHeight = Math.max(0, realNetHeight - 140 * scaleFactor);
+      let innerPosX = actualX + 70 * scaleFactor;
+      let innerPosY = actualY + 70 * scaleFactor;
+
+      // Draw the second (blue) rectangle
+      ctx.fillStyle = "blue";
+      ctx.fillRect(innerPosX, innerPosY, innerWidth, innerHeight);
+      ctx.strokeRect(innerPosX, innerPosY, innerWidth, innerHeight);
+      
+      actualX += this.netWidth * scaleFactor
+      
+
+      this.right.forEach(obj => {
+        drawRectangle(ctx, actualX, actualY, obj.width, this.netHeight, scaleFactor, "white");
+        actualX += obj.width * scaleFactor
+      });  
+
+      actualY += this.netHeight * scaleFactor
+
+      this.down.forEach(obj => {
+        drawRectangle(ctx, posX, actualY, this.width, obj.height, scaleFactor, "white");
+        actualY += obj.height * scaleFactor
+      });  
+    
+    
   }
 }
 
 const Layout = () => {
+
+  //const prototypeEinheit = new Einheit(1000, 1000, [{ type: 'N', width: 1000, height: 120 }], [{ type: 'N', width: 1000, height: 30 }], [{ type: 'N', width: 60, height: 850 }, { type: 'N', width: 30, height: 850 }], [{ type: 'N', width: 50, height: 850 }]);
+  const prototypeEinheit = new Einheit(1000, 1000, [], [], [], []);
 
   const [activeMode, setActiveMode] = useState("Fenster");
   const [inputWidth, setInputWidth] = useState(1000);
@@ -46,30 +110,16 @@ const Layout = () => {
   const [clickedEinheit, setClickedEinheit] = useState(null);
   const [scaleFactor, setScaleFactor] = useState(1);
   const [divisionMode, setDivisionMode] = useState("horizontal");
-  const [matrixOfEinheitObjects, setMatrixOfEinheitObjects] = useState([   [new Einheit(1000, 1000)] ]);
+  const [matrixOfEinheitObjects, setMatrixOfEinheitObjects] = useState([[prototypeEinheit]]);
+  
+
+  const [verbreiterungWidth, setVerbreiterungWidth] = useState(60);
+  const [kopplungWidth, setKopplungWidth] = useState(20)
 
   const canvasRef = useRef(null);
   const divRef = useRef(null);
 
-  const resizeAndDraw = () => {
-    if (divRef.current && canvasRef.current) {
-      canvasRef.current.width = divRef.current.offsetWidth - 40;
-      canvasRef.current.height = divRef.current.offsetHeight - 40;
-    }
-    const canvas = canvasRef.current;
-    const scaleFactor = Math.min(
-      canvas.width / inputWidth,
-      canvas.height / inputHeight
-    );
-    const einheit1 = new Einheit(inputWidth, inputHeight);
-    setMatrixOfEinheitObjects([[einheit1]]);
-    let realWidth = inputWidth * scaleFactor;
-    let realHeight = inputHeight * scaleFactor;
-    let posX = (canvas.width - realWidth) / 2; // Center horizontally
-    let posY = (canvas.height - realHeight) / 2; // Center vertically
-    einheit1.drawEinheit(posX, posY, canvas, scaleFactor);
-    setScaleFactor(scaleFactor);
-  };
+  
 
   // Initial draw, rescale and if outer dimensions change
   useEffect(() => {
@@ -97,13 +147,14 @@ const Layout = () => {
       canvas.height / inputHeight
     );
 
-    const einheit1 = new Einheit(inputWidth, inputHeight);
-    setMatrixOfEinheitObjects([[einheit1]]);
+    prototypeEinheit.width = inputWidth;
+    prototypeEinheit.height = inputHeight;
+    setMatrixOfEinheitObjects([[prototypeEinheit]]);
     let realWidth = inputWidth * scaleFactor;
     let realHeight = inputHeight * scaleFactor;
     let posX = (canvas.width - realWidth) / 2; // Center horizontally
     let posY = (canvas.height - realHeight) / 2; // Center vertically
-    einheit1.drawEinheit(posX, posY, canvas, scaleFactor);
+    prototypeEinheit.drawEinheit(posX, posY, canvas, scaleFactor);
     setScaleFactor(scaleFactor);
     
   }, [inputWidth, inputHeight]);
@@ -182,6 +233,19 @@ const Layout = () => {
           y >= posY &&
           y <= posY + cumulatedHeight + realHeight
         ) {
+
+          let clickedSide = '';
+
+          if ((x - posX - cumulatedWidth) / scaleFactor <= 100 ) {
+            clickedSide = 'left';
+          } else if ((y - posY - cumulatedHeight) / scaleFactor <= 100 ) {
+            clickedSide = 'up';
+          } else if ((posX + cumulatedWidth + realWidth - x) / scaleFactor < 100) {
+            clickedSide = 'right';
+          } else if ((posY + cumulatedHeight + realHeight - y) / scaleFactor < 100) {
+            clickedSide = 'down';
+          }
+          console.log(matrixOfEinheitObjects)
           // console.log(`Einheit at index ${index} was clicked.`);
           setClickedEinheit(index);
           let deletedEinheit = row[index];
@@ -192,8 +256,8 @@ const Layout = () => {
             );
             let newWidth2 = deletedEinheit.width - newWidth1;
             if (newWidth1 >= 300 && newWidth2 >= 300 && matrixOfEinheitObjects.length === 1) {
-              const einheit1 = new Einheit(newWidth1, deletedEinheit.height);
-              const einheit2 = new Einheit(newWidth2, deletedEinheit.height);
+              const einheit1 = new Einheit(newWidth1, deletedEinheit.height, [], [], [], []);
+              const einheit2 = new Einheit(newWidth2, deletedEinheit.height,  [], [], [], []);
               updatedArray.splice(index, 1, einheit1, einheit2);
               updatedMatrix[rowIndex] = updatedArray;
             }
@@ -203,8 +267,8 @@ const Layout = () => {
             );
             let newHeight2 = deletedEinheit.height - newHeight1;
             if (newHeight1 >= 300 && newHeight2 >= 300 && row.length === 1) {
-                const einheit1 = new Einheit(deletedEinheit.width, newHeight1);
-                const einheit2 = new Einheit(deletedEinheit.width, newHeight2);
+                const einheit1 = new Einheit(deletedEinheit.width, newHeight1,  [], [], [], []);
+                const einheit2 = new Einheit(deletedEinheit.width, newHeight2,  [], [], [], []);
                 updatedArray.splice(index, 1, einheit1);
                 updatedMatrix[rowIndex] = updatedArray;
                 updatedMatrix.splice(rowIndex, 1, updatedArray, [einheit2]);
@@ -215,12 +279,40 @@ const Layout = () => {
               (y - posY) / scaleFactor
             );
             if (newHeight1 >= 300 && matrixOfEinheitObjects.length === 1 ) {
-              const einheit1 = new Einheit(deletedEinheit.width, newHeight1);
+              const einheit1 = new Einheit(deletedEinheit.width, newHeight1,  [], [], [], []);
               updatedArray.splice(index, 1, einheit1);
               updatedMatrix[rowIndex] = updatedArray;
               shouldBreak = true;
             }
-          }
+          } else if (divisionMode === "Verbreiterung") {
+                const einheit1 = deletedEinheit;
+                if (clickedSide === 'up') {
+                  einheit1.up.push({type: "V", height:verbreiterungWidth})
+                } else if (clickedSide === 'left') {
+                  einheit1.left.push({type: "V", width:verbreiterungWidth})
+                } else if (clickedSide === 'right') {
+                  einheit1.right.push({type: "V", width:verbreiterungWidth})
+                } else if (clickedSide === 'down') {
+                  einheit1.down.push({type: "V", height:verbreiterungWidth})
+                }
+                updatedArray.splice(index, 1, einheit1);
+                updatedMatrix[rowIndex] = updatedArray;
+                shouldBreak = true;
+          } else if (divisionMode === "Kopplung") {
+            const einheit1 = deletedEinheit;
+            if (clickedSide === 'up') {
+              einheit1.up.push({type: "K", height:kopplungWidth})
+            } else if (clickedSide === 'left') {
+              einheit1.left.push({type: "K", width:kopplungWidth})
+            } else if (clickedSide === 'right') {
+              einheit1.right.push({type: "K", width:kopplungWidth})
+            } else if (clickedSide === 'down') {
+              einheit1.down.push({type: "K", height:kopplungWidth})
+            }
+            updatedArray.splice(index, 1, einheit1);
+            updatedMatrix[rowIndex] = updatedArray;
+            shouldBreak = true;
+      }
         }
         cumulatedWidth += realWidth;
         cumulatedHeight += realHeight;
@@ -360,8 +452,8 @@ const Layout = () => {
                 "vertical",
                 "Abschneiden",
                 "Kopplung",
-                "Statikkopplung",
                 "Verbreiterung",
+                "Gap"
               ].map((buttonName) => (
                 <button
                   style={{ width: '150px' }}
