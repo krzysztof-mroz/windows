6339ab5dfa790ae254027b7cf31f265967adcf8f
Krzysztof Mróz <krzysztof.mroz@me.com>
2024-01-21 17:58:27 +0100

    Aluplast

--- components/ui/aluplastdiv.js
+++ components/ui/aluplastdiv.js
@@ -39,7 +39,7 @@ function aluplastdiv() {
     <div className="flex flex-wrap justify-around  w-100  tc mb1 mt3 ">
 
 
-      
+
       <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
         <div className="w-100 w-50-l ma1 pa2">
           <img src="./pics/profiles/aluplast_ideal_4000.png"></img>
@@ -80,6 +80,114 @@ function aluplastdiv() {
           <img
             style={{ maxWidth: 450, maxHeight: 550 }}
             src="./pics/sections/aluplast4000.png"
+          ></img>
+        </div>
+      </div>
+
+
+      <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
+        <div className="w-100 w-50-l ma1 pa2">
+          <img src="./pics/profiles/aluplast_ideal_5000.png"></img>
+        </div>
+        <div className="w-100 w-40-l ma1 pa2 tl">
+          <h5 className="gray">Aluplast Ideal 5000, 5 Kammer, Mitteldichtung</h5>
+          <div className="gray f6 tl">
+            <ul>
+              <li>Bautiefe 70 mm</li>
+              <li>5-Kammer-System als Standardkombination</li>
+              <li>
+              Wärmedämmeigenschaften der Profile Uf-Wert = 1,1 W/m²K (mit thermisch verbessertem Stahl)
+              </li>
+              <li>Verglasungsstärke bis 41 mm</li>
+              <li>Schalldämmung bis 47 dB (bis Schallschutzklasse V)</li>
+              <li>
+                2-fache Designvielfalt im Flügel 
+              </li>
+              <li>Mitteldichtungssystem mit in zahlreichen Dekorvarianten verfügbar</li>
+            </ul>
+          </div>
+        </div>
+        <div className="w-100 w-50-l ma1 pa2">
+        <img src="./pics/profiles/aluplast_ideal_5000_tuer.png"></img>
+        </div>
+
+        <div className="w-100 w-40-l ma1 pa2">
+          <img
+            style={{ maxWidth: 450, maxHeight: 550 }}
+            src="./pics/sections/aluplast4000.png"
+          ></img>
+        </div>
+      </div>
+
+
+      <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
+        <div className="w-100 w-50-l ma1 pa2">
+          <img src="./pics/profiles/aluplast_ideal_7000.png"></img>
+        </div>
+        <div className="w-100 w-40-l ma1 pa2 tl">
+          <h5 className="gray">Aluplast Ideal 7000, 6 Kammer</h5>
+          <div className="gray f6 tl">
+            <ul>
+              <li>85mm Bautiefe</li>
+              <li>Profil Uf=1,1 W/m²K</li>
+              <li>6-Kammer-System</li>
+              <li>
+              Uw=0,86 W/m²K mit Standard-Dreifach-Verglasung mit Ug=0,6 und Psi=0,040 W/mK
+              </li>
+              <li>Verglasungsstärke bis 51 mm</li>
+              <li>Schalldämmung bis 46 dB (bis Schallschutzklasse IV)</li>
+              <li>
+                <li>bis zu RC2 Einbruchschutz</li>
+                mit zahlreichen Dekorfolien erhältlich
+              </li>
+              <li>nicht sichtbare Entwässerung möglich</li>
+              <li>flächenversetzte Variante</li>
+            </ul>
+          </div>
+        </div>
+        <div className="w-100 w-50-l ma1 pa2">
+        <img src="./pics/profiles/aluplast_ideal_7000_2.png"></img>
+        </div>
+
+        <div className="w-100 w-40-l ma1 pa2">
+          <img
+            style={{ maxWidth: 450, maxHeight: 550 }}
+            src="./pics/sections/aluplast7000.png"
+          ></img>
+        </div>
+      </div>
+
+
+
+
+      <div className="flex flex-wrap justify-around w-100 ba b--moon-gray ma2 pa3">
+        <div className="w-100 w-50-l ma1 pa2">
+          <img src="./pics/profiles/aluplast_ideal_8000.png"></img>
+        </div>
+        <div className="w-100 w-40-l ma1 pa2 tl">
+          <h5 className="gray">Aluplast Ideal 8000, 6 Kammer, Mitteldichtung</h5>
+          <div className="gray f6 tl">
+            <ul>
+              <li>Bautiefe 85 mm</li>
+              <li>6-Kammer-System</li>
+              <li>Profil Uf=1,0 W/m²K</li>
+              <li>
+              Uw von bis zu 0,67 W/m²K möglich
+              </li>
+              <li>Verglasungsstärke bis 51 mm</li>
+              <li>Schalldämmung bis 47 dB (bis Schallschutzklasse V)</li>
+              <li>Mitteldichtungssystem mit in zahlreichen Dekorvarianten verfügbar</li>
+            </ul>
+          </div>
+        </div>
+        <div className="w-100 w-50-l ma1 pa2">
+        <img src="./pics/profiles/aluplast_ideal_8000_tuer.png"></img>
+        </div>
+
+        <div className="w-100 w-40-l ma1 pa2">
+          <img
+            style={{ maxWidth: 450, maxHeight: 550 }}
+            src="./pics/sections/aluplast8000.png"
           ></img>
         </div>
       </div>
