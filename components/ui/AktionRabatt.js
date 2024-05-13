31b6eaae03a4b9aa0bfd79c17cd1edaed794c3eb
Krzysztof Mróz <krzysztof.mroz@me.com>
2024-05-13 09:55:35 +0200

    Mairabatt

--- components/ui/AktionRabatt.js
+++ components/ui/AktionRabatt.js
@@ -7,12 +7,12 @@ const AktionRabatt = () => {
           <div className="w-100">
             <h2 className="f3 f2-m f1-l b tc">Mai-Rabattaktion</h2>
             <p className="lh-copy tc">
-            Bestellen Sie zwischen 30 April und 12 Mai 2024 und erhalten Sie <span className="b">10% Sonderrabatt</span> auf Salamander und Aluplast Fenster
+            Benutzen Sie bei Ihrer Anfrage Rabattcode MAIRABATT <span className="b">und erhalten Sie 10% Sonderrabatt</span> auf Salamander und Aluplast Fenster
             </p>
           </div>
           <div className="w-100">
             <p className="lh-copy i f7 tc"> {/* Kleinere Schriftgröße und zentrierter Text */}
-              *Diese Aktion gilt nur für Angebote ab dem 30 April 2024
+              *Diese Aktion gilt nur für Angebote ab dem 13 Mai 2024
             </p>
           </div>
         </div>
